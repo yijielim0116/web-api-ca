0dcb3a43a955d98c68ccc211715ed924d6cd4758
Yi Jie Lim <yijielim95@Yi-Jies-MacBook-Air.local>
2025-12-14 10:36:17 +0000

    getMovies now using our Movies API

--- react-movies/src/api/tmdb-api.jsx
+++ react-movies/src/api/tmdb-api.jsx
@@ -1,6 +1,6 @@
 export const getMovies = () => {
   return fetch(
-    `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    `http://localhost:8080/api/movies/discover`
   ).then((response) => {
     if (!response.ok) {
       return response.json().then((error) => {
@@ -13,7 +13,6 @@ export const getMovies = () => {
       throw error
   });
 };
-
 
 export const getMovie = (args) => {
   const [, idPart] = args.queryKey;
